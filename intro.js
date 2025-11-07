baac642de28ead3a332d2a2a0fb97af2b49feb1c
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-07 04:17:01 -0500

    Initial commit with js and py files. Plan is to rewrite the python code in js to function the same

---
+++ intro.js
@@ -0,0 +1,7 @@
+//Difference in syntax between JS and Python
+
+/* def greet(name):
+    print(f"Hello,{name}")
+    return
+
+greet("Cloud Engineer!"), all this in python */
---
+++ intro.py
@@ -0,0 +1,6 @@
+#Difference in syntax between python and js
+def greet(name):
+    print(f"Hello,{name}")
+    return
+
+greet("Cloud Engineer!")
