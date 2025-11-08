a5ee9a59db8e509dae6c308ce4d064ec401888f1
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-08 04:53:40 -0500

    Seperating arguments in the forLoop require semi-colons

--- forLoops.js
+++ forLoops.js
@@ -1,5 +1,5 @@
 let regions = ['us-west-1','us-west-2','us-east-1','us-east-2'];
 
-for (let i = 0; i < regions.length, i++) {
+for (let i = 0; i < regions.length; i++) {
     console.log("Deploying to region: " + regions[i]);
 }
