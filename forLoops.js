7660fea6fa5c5029b405ac4a5ed19973921abe11
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-08 05:01:40 -0500

    Practice with forLoops

--- forLoops.js
+++ forLoops.js
@@ -3,3 +3,9 @@ let regions = ['us-west-1','us-west-2','us-east-1','us-east-2'];
 for (let i = 0; i < regions.length; i++) {
     console.log("Deploying to region: " + regions[i]);
 }
+
+let cloudServices = ['S3','IAM','CloudFront','DynamoDB','EC2'];
+
+for (let i = 0; i < cloudServices.length; i++) {
+    console.log('Configuring the following service: ' + cloudServices[i]);
+}
