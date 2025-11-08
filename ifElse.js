43eb10fc97c65cbbb093798f60099e3753bf7ae6
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-08 01:18:35 -0500

    Exercise with if/else statements

--- ifElse.js
+++ ifElse.js
@@ -1,6 +1,6 @@
 // If / Else Statements
 
-let cloudProvider = "aws"
+let cloudProvider = "aws";
 
 if (cloudProvider === "aws") {
     console.log("We're using Amazon Web Services.");
@@ -9,3 +9,10 @@ if (cloudProvider === "aws") {
 };
 
 // Exercise - IF / Else statement for Terraform
+let iacProvider = "Terraform";
+
+if (iacProvider === "Terraform") {
+    console.log("We're using Terraform.");
+} else {
+    console.log("We are not using Terraform.");
+};
